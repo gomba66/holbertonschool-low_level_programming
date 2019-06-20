f9cf07962032f3b8e8087e037045ac9dce398142
miguel-dev <danielalejo66@hotmail.com>
2019-06-20 19:13:33 +0000

    This is a code in C that compare various numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	if (n > 5)
+	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
-	else if (n !=  0 && n < 6)
+	else if (last !=  0 && last < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
