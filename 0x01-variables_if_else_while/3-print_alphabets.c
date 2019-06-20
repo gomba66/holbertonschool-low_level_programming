15c9f63d56b1754f379af3826442008800d97109
miguel-dev <danielalejo66@hotmail.com>
2019-06-20 20:52:59 +0000

    This is a code in C for print a alphabet with putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	char y;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'A'; y <= 'Z'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
