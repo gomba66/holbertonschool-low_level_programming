9d670f99d283223ac43af45ab7e14ab84717d4a3
miguel-dev <danielalejo66@hotmail.com>
2019-06-20 21:59:31 +0000

    This is a program in c for print numbers in order 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numerito = 0;
+
+	while (numerito < 10)
+	{
+		printf("%d", numerito);
+		numerito++;
+	}
+	printf("\n");
+	return (0);
+}
