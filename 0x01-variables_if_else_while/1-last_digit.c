b885d44292dbb79b97d1794c268651c89e4bb8f1
miguel-dev <danielalejo66@hotmail.com>
2019-06-20 18:25:26 +0000

    This is a code in C with (if) conditionals

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,10 +8,23 @@
  */
 int main(void)
 {
-	int n;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+	last = n % 10;
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
+	else if (n !=  0 && n < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is greater than and is 0\n", n, last);
+	}
 	return (0);
+
 }
