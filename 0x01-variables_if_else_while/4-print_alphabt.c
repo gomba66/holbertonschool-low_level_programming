3c7345d75114156cb8cb42080b9e9b29f632fdcb
miguel-dev <danielalejo66@hotmail.com>
2019-06-20 21:41:30 +0000

    i fixed the condition (if)

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,12 @@ int main(void)
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		if(ch != 'q' || ch != 'e' )
+		if (ch == 'q' || ch == 'e')
 		{
-		putchar(ch);
+		}
+		else
+		{
+			putchar(ch);
 		}
 	}
 	putchar('\n');
