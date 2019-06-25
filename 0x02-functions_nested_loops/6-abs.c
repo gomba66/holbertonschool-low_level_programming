bc77bf80a393b04386febd44bb43fd13ab3e28ff
miguel-dev <danielalejo66@hotmail.com>
2019-06-25 20:37:19 +0000

    This is a program for print the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - Entry point
+ * This is a function for print absolute value of a number
+ * @n: is the variable
+ * Return: int value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
