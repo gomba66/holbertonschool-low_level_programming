06761aceb8be84f6d61462a6ddfdefebf65c7d1f
miguel-dev <danielalejo66@hotmail.com>
2019-06-25 01:00:12 +0000

    This program print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
