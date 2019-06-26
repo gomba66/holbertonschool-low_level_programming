a0d45dfeb9ca59711f632cd89f2f28fb3dc082f3
miguel-dev <danielalejo66@hotmail.com>
2019-06-26 22:27:08 +0000

    i added a tables of nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,21 @@ void times_table(void)
 		for(op2 = 0; op2 <= 9; op2++)
 		{
 			res = op1 * op2;
-			_putchar(res + '0');
-			if (res >= 9)
+			if (res < 10)
+			{
+				if (res < 9 && res != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(res + '0');
+				_putchar(',');
+			}
+			else
 			{
 				_putchar((res/10) + '0');
 				_putchar((res%10) + '0');
+				_putchar(',');
 				_putchar(' ');
 			}
 		}
