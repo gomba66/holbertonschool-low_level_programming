6d8e7ba26d245b078f78b28af86d4a1f82b25940
gomba66 <danielalejo66@hotmail.com>
2019-06-26 04:49:45 +0000

    This is a program for add two numbers[

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ * Return: the result of add two integers
+ * @a: is a integer
+ * @b: is other integer
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
