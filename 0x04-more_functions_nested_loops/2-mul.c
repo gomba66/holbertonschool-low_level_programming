164fff4425b91c1e66ddb0421eba98a7508ae6ed
miguel-dev <danielalejo66@hotmail.com>
2019-06-27 15:51:35 +0000

    This is a program for multiplicate 2 digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Entry point
+ * This is a function for multiplicate 2 digits
+ * @a: is a first digit.
+ * @b: is the second digit.
+ */
+int mul(int a, int b)
+{
+	int n;
+	n = a * b;
+	return (n);
+}
