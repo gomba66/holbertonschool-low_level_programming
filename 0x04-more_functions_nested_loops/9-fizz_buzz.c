eebdf3fbdfa8d0e6e04328faca25fc1073f3dbca
miguel-dev <danielalejo66@hotmail.com>
2019-06-28 03:32:18 +0000

    This is a program for print FIZZ and BUZZ with numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ */
+int main(void)
+{
+	int num1 = 1;
+
+	while (num1 <= 100)
+	{
+		if (num1 % 3 == 0 && num1 % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num1 % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num1 % 5 == 0 && num1 == 100)
+		{
+			printf("Buzz");
+		}
+		else if (num1 % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num1);
+		}
+		num1++;
+	}
+	printf("\n");
+	return (0);
+}
