fe61a91c48615339f13bf1752c18e8005d203234
miguel-dev <danielalejo66@hotmail.com>
2019-07-02 21:58:49 +0000

    I changed a little thing

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,15 @@
  */
 void puts2(char *str)
 {
+	int i = 0;
 	while (*str != '\0')
 	{
-		if (*str % 2 == 0)
+
+		if (i % 2 == 0)
 		{
-			_putchar(*str);
+			_putchar(i + '0');
 		}
+		i++;
 		str++;
 	}
 	_putchar('\n');
