45d3c8d41275e9967facf89bea703360b24fd37f
miguel-dev <danielalejo66@hotmail.com>
2019-07-02 21:15:19 +0000

    This is a program for print a string, but only multiples of two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2 - Entry point
+ * This is a function for print characters (out of 2 of a string)
+ * @str: This is a parameter that entry in the function
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
