25e8655c28f2fbe3c12d9bbf17f35605ebc4e5d8
miguel-dev <danielalejo66@hotmail.com>
2019-07-02 21:13:31 +0000

    This is a program for change a string in reverse from memory

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rev_string - Entry point
+ * This is a function for put a string in reverse from memory
+ * @s: This is a string that join in the function
+ */
+void rev_string(char *s)
+{
+	int x, y;
+	char i[y];
+
+	while (s[x] != '\0')
+	{
+		x++;
+		y++;
+	}
+	y--;
+	while (y != -1)
+	{
+		x++;
+		i[x] = s[x];
+		y--;
+	}
+	i = s;
+}
