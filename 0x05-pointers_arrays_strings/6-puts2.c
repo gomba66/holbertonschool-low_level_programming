0cd79f0d0f9cd85c186e51b8cfb93dbdf8dcb5ea
miguel-dev <danielalejo66@hotmail.com>
2019-07-02 22:03:06 +0000

    FIXEED

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,14 @@
 void puts2(char *str)
 {
 	int i = 0;
-	while (*str != '\0')
+	while (str[i] != '\0')
 	{
 
 		if (i % 2 == 0)
 		{
-			_putchar(i + '0');
+			_putchar(str[i]);
 		}
 		i++;
-		str++;
 	}
 	_putchar('\n');
 }
