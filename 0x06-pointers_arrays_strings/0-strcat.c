6a0d61c612e91e6854f66b16bad370cd21cb7867
miguel-dev <danielalejo66@hotmail.com>
2019-07-04 18:15:29 +0000

    I FIXED BETTY

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,16 @@ char *_strcat(char *dest, char *src)
 {
 	int z = 0;
 	char *new = dest;
+	int i = 0;
+	char *new2 = src;
+	int a = z;
+	int b = 0;
 
 	while (*new != '\0')
 	{
 		z++;
 		new++;
 	}
-
-	int i = 0;
-	char *new2 = src;
-	int a = z;
-	int b = 0;
 
 	while (*new2 != '\0')
 	{
