9daf9074d6b47852e5c53da2e4d4cd1e5009243b
miguel-dev <danielalejo66@hotmail.com>
2019-07-04 23:31:32 +0000

    This is a program for comparate strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strcmp - Entry point
+ * @s1: This is the first string
+ * @s2: This is the second string
+ * Return: result from the rest of s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (char) (*s1) - (char)(*s2);
+}
