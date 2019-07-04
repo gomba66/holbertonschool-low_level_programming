965f23a2af2c626b709418f521d95f2211083614
miguel-dev <danielalejo66@hotmail.com>
2019-07-04 21:50:04 +0000

    This is a program for concatenate 2 strings with a limit in the parameter

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strncat - Entry point
+ * @dest: is the first string that join as parameter
+ * @src: is the second string that join as parameter
+ * @n: is the mount of bytes declarated as parameter
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	int a = 0;
+	while (src[a] != '\0' && n > 0)
+	{
+		dest[i] = src[a];
+		a++;
+		i++;
+		n--;
+	}
+
+	return (dest);
+}
