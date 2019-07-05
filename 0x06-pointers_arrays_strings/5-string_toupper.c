be2f18ec3c58d45933e351eb3ce6cfcb0adde999
miguel-dev <danielalejo66@hotmail.com>
2019-07-05 03:27:37 +0000

    This is a program for change lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * string_toupper - Entry point
+ * This is a function for change the lowercase to uppercase
+ * @s: this is the string that join and return
+ * Return: s - string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 122 && s[i] >= 97)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
