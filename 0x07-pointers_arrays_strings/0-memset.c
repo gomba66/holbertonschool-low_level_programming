a7f93c9e5463d10c8a5770cec9675c1c746e4e06
gomba66 <danielalejo66@hotmail.com>
2019-07-08 19:38:52 +0000

    This is the new project of arrays, pointers and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _memset - Entry point
+ * This is a function for fill memory with a constant byte.
+ * @s: This is the buffer
+ * @b: This is the value that to be replaced
+ * @n: This is the amount of times that the value will be replaced
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
