69609840795a7812c1213c866d9f2ac6812d1fe6
gomba66 <danielalejo66@hotmail.com>
2019-07-09 00:27:50 +0000

    This is a function for comparate characters and print the number of coincidences

---
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
+
+	n = strspn(s, f);
+	printf("%u\n", n);
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,5 @@
+#include "holberton.h"
+/**
+ * _strspn - Entry point
+ * This is a function that get the length of a prefix substring
+ * @s: this is 
