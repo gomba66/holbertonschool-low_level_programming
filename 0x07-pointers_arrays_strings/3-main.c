864e38c0d06a0b69bbf3ddf75bf8c22d0b5e431c
gomba66 <danielalejo66@hotmail.com>
2019-07-09 03:29:31 +0000

    This is a program for comparate characters and determine the number of char equals

--- 0x07-pointers_arrays_strings/3-main.c
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -1,18 +1,18 @@
+#include "holberton.h"
 #include <stdio.h>
-#include <string.h>
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char *s = "hello, world";
 	char *f = "oleh";
 	unsigned int n;
 
-	n = strspn(s, f);
+	n = _strspn(s, f);
 	printf("%u\n", n);
 	return (0);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,4 +2,30 @@
 /**
  * _strspn - Entry point
  * This is a function that get the length of a prefix substring
- * @s: this is 
+ * @s: this is an array.
+ * @accept: this is the second array
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, a;
+	int b = 0;
+
+	while (s[i] > 0)
+	{
+		while (accept[a])
+		{
+			if (s[i] == accept[a])
+			{
+				b++;
+			}
+			a++;
+		}
+		i++;
+		if (i > b)
+		{
+			break;
+		}
+	}
+	return (b);
+}
