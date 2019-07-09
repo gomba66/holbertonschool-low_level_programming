016a185a1e39517b9548e1b90f50706b0fa5552b
gomba66 <danielalejo66@hotmail.com>
2019-07-09 03:54:23 +0000

    This is a function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strstr - Entry point
+ * @haystack: the char for print
+ * @needle: null character or needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (needle[i] != haystack[i])
+				break;
+		}
+		if (!needle[i])
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
