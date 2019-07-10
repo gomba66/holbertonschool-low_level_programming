6a63fc9b09509296469f0a1c20b942018c7c8d32
gomba66 <danielalejo66@hotmail.com>
2019-07-10 19:00:56 +0000

    Starting a new topyc: Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - Entry point
+ * This is a function for print a string using recursions
+ * Return: the string, character per character
+ * @s: is the string
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	if (s[i] != '\0')
+	{
+        	_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
