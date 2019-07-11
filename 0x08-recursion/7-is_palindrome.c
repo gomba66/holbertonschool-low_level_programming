dc02d99a563dad2a296b7d2a3c9149d04e0fcf90
gomba66 <danielalejo66@hotmail.com>
2019-07-11 04:59:31 +0000

    This is a program for comparate characters in a array

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * is_palindrome - Entry point
+ * This is a function for comparate 2 strings
+ * @s: Is the first string
+ * Return: If the strings be identical 1, otherwise return 0.
+ */
+int is_palindrome(char *s)
+{
+	if (s1 == '\0')
+	{
+		return (0);
+	}
+	else if (s2 == '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
