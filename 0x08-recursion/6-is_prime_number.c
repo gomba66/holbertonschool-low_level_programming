ac968bb16a93c012f0c56e1149fb778d6c5d9fb1
gomba66 <danielalejo66@hotmail.com>
2019-07-11 05:06:24 +0000

    This is other program

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * is_prime_number - Entry point
+ * This is a function for comparate 2 strings
+ * @s: Is the first string
+ * Return: If the strings be identical 1, otherwise return 0.
+ */
+int is_prime_number(int n)
+{
+	if (n == '\0')
+	{
+		return (0);
+	}
+	else if (n == '\0')
+	{
+		return (1);
+	}
+	return (1);
+}
