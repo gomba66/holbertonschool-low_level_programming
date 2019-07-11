0b1b9b336466012e4ac9ac528c63b0d84927c7a4
gomba66 <danielalejo66@hotmail.com>
2019-07-11 00:06:24 +0000

    This is the function for print the factorial number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * factorial - Entry point
+ * This is a function for print the factorial of a number
+ * @n: This is the number that join from main.
+ * Return: -1 if the function is less than 0.
+ */
+int factorial(int n)
+{
+	if(n < 0)
+	{
+		return(-1);
+	}
+	if(n == 0)
+	{
+		return(1);
+	}
+
+	return (n * factorial(n - 1));
+}
