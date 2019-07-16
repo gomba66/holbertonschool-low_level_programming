476c5b578a7a1ff9382a5f790e410e0e63b6352f
gomba66 <danielalejo66@hotmail.com>
2019-07-16 04:23:59 +0000

    This is a program for add two numbers, but is incomplete

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This is a program for add two positive numbers
+ * Return: Result of add.
+ * @argc: This is the number of strings or arguments
+ * @argv: This is the string or arguments passed.
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	return(0);
+}
