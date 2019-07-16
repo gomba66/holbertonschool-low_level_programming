2b1d8abb002586f0904728030c487e7c09400bcd
gomba66 <danielalejo66@hotmail.com>
2019-07-16 20:46:41 +0000

    This is a new program for copy a string and return the copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - Entry point
+ * This is a program for copy a string and return the copy
+ * Return: Copy of a string.
+ * @str: string.
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int n = 0;
+	char *x;
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		while (str[i] != '\0')
+		{
+			n++;
+			i++;
+		}
+		i = 0;
+		x = malloc(sizeof(char) * n);
+		if (x == '\0')
+		{
+			return ('\0');
+		}
+		else
+		{
+			while (i < n)
+			{
+				x[i] = str[i];
+				i++;
+			}
+			return (x);
+		}
+	}
+}
