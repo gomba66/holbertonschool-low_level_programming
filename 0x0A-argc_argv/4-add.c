a7407bb62f37e4761dee8d314dcec83c0640009f
gomba66 <danielalejo66@hotmail.com>
2019-07-16 04:51:26 +0000

    I fixed betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,13 +19,14 @@ int main(int argc, char *argv[])
 	else
 	{
 		int x = 0, y = 0;
-		if(isdigit(argv[x][y]))
+
+		if (isdigit(argv[x][y]))
 		{
-			for(x = 0; x < argc; x++)
+			for (x = 0; x < argc; x++)
 			{
-				for(y = 0; argv[x][y] != '\0'; y++)
+				for (y = 0; argv[x][y] != '\0'; y++)
 				{
-					if(!isdigit(argv[x][y]))
+					if (!isdigit(argv[x][y]))
 					{
 						printf("Error\n");
 						return (1);
