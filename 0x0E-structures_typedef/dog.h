62596d4be3086bfeaaa216d08bbcfa122f95df5f
gomba66 <danielalejo66@hotmail.com>
2019-07-23 02:31:03 +0000

    i fixed betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Structure of a dog
+ * @name: Is the name of the dog
+ * @age: Is the age of the dog
+ * @owner: Is the owner of the dog
+ */
 struct dog
 {
 	char *name;
