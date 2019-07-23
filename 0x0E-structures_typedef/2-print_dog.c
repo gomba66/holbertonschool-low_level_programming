abfe74ef1362f0ed4db88590109645ecc9869bd9
gomba66 <danielalejo66@hotmail.com>
2019-07-23 23:02:18 +0000

    I fixed the betty style

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,8 +23,8 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-       	printf("Name: %s\n",d->name);
-	printf("Age: %f\n",d->age);
-	printf("Owner: %s\n",d->owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
