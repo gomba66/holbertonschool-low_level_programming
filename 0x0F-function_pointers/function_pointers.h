78790b1bc2e7c5ea58f81987eccd629a5f35201d
gomba66 <danielalejo66@hotmail.com>
2019-07-25 04:28:22 +0000

    [fix] I removed a prototype in the header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,5 @@
 #define FUNCT_TO_POINTERS
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
