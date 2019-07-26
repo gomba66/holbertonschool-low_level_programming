2e15c729a8824a05cc47bb8c2b2c0aec4c6440a1
gomba66 <danielalejo66@hotmail.com>
2019-07-26 21:02:04 +0000

    I added the function for add x numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - Entry point
+ * This is a variadic function for add multiples args
+ * @n:Is the number of items that entry like args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		int sum = 0;
+		unsigned int x;
+		va_list numeros;
+
+		va_start(numeros,n);
+		for (x = 0; x < n; x++)
+		{
+			sum = sum + va_arg(numeros,int);
+		}
+		va_end(numeros);
+		return (sum);
+	}
+}
