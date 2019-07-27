85aa6939597ec636e344bd6c9281de35eb6d7288
gomba66 <danielalejo66@hotmail.com>
2019-07-27 21:42:30 +0000

    Practice with structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,8 @@
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * struct dog - Structure of a dog
- * @name: Is the name of the dog
- * @age: Is the age of the dog
- * @owner: Is the owner of the dog
- */
-struct dog
-{
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+struct dog{
 	char *name;
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 #endif
