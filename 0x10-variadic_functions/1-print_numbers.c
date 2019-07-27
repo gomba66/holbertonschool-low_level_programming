45e4f60fa1c9c096777833e1f191708b49cb8dcb
gomba66 <danielalejo66@hotmail.com>
2019-07-27 01:20:55 +0000

    I added a new function for print numbers separated with a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - Entry point
+ * This is a function for print a list of items
+ * @separator: this is the separator between items
+ * @n: Is the mount of items that are passed to the function
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numbers;
+
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		int num;
+
+		num = va_arg(numbers, int);
+		printf("%d", num);
+		if (separator != NULL)
+		{
+			if (i < (n - 1))
+			{
+				printf("%s", separator);
+			}
+		}
+		if (i == (n - 1))
+		{
+			printf("\n");
+		}
+	}
+	va_end(numbers);
+}
