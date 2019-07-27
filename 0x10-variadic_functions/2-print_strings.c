0c51982a005485dd30af6aaa61fb20e95dbea85f
gomba66 <danielalejo66@hotmail.com>
2019-07-27 20:50:12 +0000

    FIX2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,10 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 			}
 		}
-		if (i == (n - 1))
-		{
-			printf("\n");
-		}
 	}
+	printf("\n");
 	va_end(strings);
 }
