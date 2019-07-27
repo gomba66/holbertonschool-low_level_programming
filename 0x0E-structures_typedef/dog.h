3ea6939fb661fdefbf47d7e21557aff1f2cbf66d
gomba66 <danielalejo66@hotmail.com>
2019-07-27 21:51:35 +0000

    I fixed the betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_STRUCT
 #define DOG_STRUCT
-struct dog{
+/**
+ * struct dog - Is a struct of a dog
+ * @name: Is the name of a dog
+ * @age: Is the name of a dog
+ * @owner: Is the name of owner dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
