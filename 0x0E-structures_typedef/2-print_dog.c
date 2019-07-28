a77d98718993e5f52d0db8916eb65d7aeeb0c003
gomba66 <danielalejo66@hotmail.com>
2019-07-28 00:26:51 +0000

    I added a new condition

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,21 +7,24 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
+	if(d != NULL)
 	{
-		printf("Name: %p\n", d->name);
-	}
-	else
-	{
-	printf("Name: %s\n", d->name);
-	}
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-	{
-		printf("Owner: %p\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: %s\n", d->owner);
+		if (d->name == NULL)
+		{
+			printf("Name: %p\n", d->name);
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+		{
+			printf("Owner: %p\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
 	}
 }
