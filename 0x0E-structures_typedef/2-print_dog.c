153636ffeef0bce60845170e10cecd2b123397ef
gomba66 <danielalejo66@hotmail.com>
2019-07-28 00:24:12 +0000

    I fixed conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,21 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d->name == NULL)
+	{
+		printf("Name: %p\n", d->name);
+	}
+	else
+	{
 	printf("Name: %s\n", d->name);
+	}
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+	{
+		printf("Owner: %p\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
