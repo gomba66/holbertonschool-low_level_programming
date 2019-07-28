b448278133f536d223c9894d7b1c613987fb65bb
gomba66 <danielalejo66@hotmail.com>
2019-07-28 18:11:40 +0000

    I added a structure and new prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef VARIADIC_FUNCT
 #define VARIADIC_FUNCT
+#include <stdarg.h>
+typedef struct types{
+	char *c;
+	void (*f)();
+} tp;
+void print_char(va_list x);
+void print_int(va_list x);
+void print_float(va_list x);
+void print_string(va_list x);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
