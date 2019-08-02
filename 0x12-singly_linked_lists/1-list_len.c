43f0b8cf9742e049c0884496f9bb94fa44908bf1
gomba66 <danielalejo66@hotmail.com>
2019-08-02 03:23:16 +0000

    Task 1: I added the function 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - This is a function for count the nodes and return this counter
+ * @h: This is the direccion structure
+ * Return: total of nodes.
+ */
+size_t list_len(const list_l *h)
+{
+	int x;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
