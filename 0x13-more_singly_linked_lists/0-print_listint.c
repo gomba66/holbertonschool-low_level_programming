f13b04913115af337ac1ab522a7d92a091176ae5
gomba66 <danielalejo66@hotmail.com>
2019-08-06 21:36:26 +0000

    Function that print all positions in the list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_listint - Function that print all elements of a list.
+ * @h: This is the structure
+ * Return: the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while(h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+
+
+	return (x);
+}
