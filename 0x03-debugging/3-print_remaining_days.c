bdb77e7d8546d10b262504845fd5581bed7a9bca
gomba66 <danielalejo66@hotmail.com>
2019-08-06 12:23:38 -0500

    FIX the betty Style

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,36 +2,41 @@
 #include "holberton.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0  && !(year % 100 == 0)) || (year % 100 ==0 && year % 400 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+	int m = month;
+	int d = day;
+	int y = year;
+
+	if ((y % 4 == 0 && !(y % 100 == 0)) || (y % 100 == 0 && y % 400 == 0))
+	{
+		if (m >= 2 && d >= 60)
+		{
+			d++;
+		}
+		printf("Day of the year: %d\n", d);
+		printf("Remaining days: %d\n", 366 - d);
+	}
+	else
+	{
+		if (m == 2 && d == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", m, d - 31, y);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", d);
+			printf("Remaining days: %d\n", 365 - d);
+		}
+	}
+
 }
