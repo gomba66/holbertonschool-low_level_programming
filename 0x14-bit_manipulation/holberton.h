3428de0be02968425b6e2ad52780e730d5224969
gomba66 <danielalejo66@hotmail.com>
2019-08-09 00:42:13 -0500

    I added a new prototype

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,8 @@
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
 #include <stdlib.h>
+#include <stdio.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int  n);
 #endif
