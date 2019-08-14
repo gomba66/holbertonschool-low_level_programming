a9ab9b8dfce3410d961505b930653fd51dbc4420
gomba66 <danielalejo66@hotmail.com>
2019-08-14 05:05:47 +0000

    I added the header file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef FILEIO
+#define FILEIO
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
