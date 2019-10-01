54f27218f593611fa0c9dc79d394e334838f68ff
gomba66 <danielalejo66@hotmail.com>
2019-10-01 19:59:35 +0000

    I fixed the program

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,21 +5,20 @@
  */
 int main(void)
 {
-	unsigned int long n1 = 0, n2 = 1, count = 0, fib = 1;
+	unsigned int long n1 = 0, n2 = 1, count = 0, fib = 1, sum = 0;
 
 	while (count < 100)
 	{
 		fib = n1 + n2;
 		if (fib % 2 == 0 && fib < 4000000)
-			printf("%lu", fib);
-		if (count < 49 && (fib % 2 == 0 && fib < 3524578))
 		{
-			printf(", ");
+			sum = sum + fib;
 		}
 		n1 = n2;
 		n2 = fib;
 		count++;
 	}
+	printf("%lu", sum);
 	printf("\n");
 	return (0);
 }
