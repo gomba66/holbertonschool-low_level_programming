976fa33176d7e58af72c6c31e8c18c2e091a9ca8
gomba66 <danielalejo66@hotmail.com>
2019-10-01 04:41:32 +0000

    I fixed the print function [debugging task]

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,31 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-
-	int m = month;
-	int d = day;
-	int y = year;
-
-	if ((y % 4 == 0 && !(y % 100 == 0)) || (y % 100 == 0 && y % 400 == 0))
+	if (year % 4 == 0)
 	{
-		if (m >= 2 && d >= 60)
+		if (month >= 2 && day >= 60)
 		{
-			d++;
+			day++;
 		}
-		printf("Day of the year: %d\n", d);
-		printf("Remaining days: %d\n", 366 - d);
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (m == 2 && d == 60)
+		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", m, d - 31, y);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n", d);
-			printf("Remaining days: %d\n", 365 - d);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-
 }
