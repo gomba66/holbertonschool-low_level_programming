b5019580f3357954f46510adb3179f54bface142
gomba66 <danielalejo66@hotmail.com>
2019-10-01 08:08:12 +0000

    I finished the task 9 for print the multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,37 @@
 
 void times_table(void)
 {
-	int op1, op2, res;
+	int op1, op2, res, dig1, dig2;
 
 	for (op1 = 0; op1 <= 9; op1++)
 	{
-		for(op2 = 0; op2 <= 9; op2++)
+		for (op2 = 0; op2 <= 9; op2++)
 		{
 			res = op1 * op2;
 			if (res < 10)
 			{
-				if (res < 9 && res != 0)
+				_putchar(res + '0');
+			}
+			else
+			{
+				dig1 = res / 10;
+				dig2 = res % 10;
+				_putchar(dig1 + '0');
+				_putchar(dig2 + '0');
+
+			}
+			if (op2 < 9)
+			{
+				_putchar(',');
+				if ((op1) * (op2 + 1) < 10)
 				{
 					_putchar(' ');
 					_putchar(' ');
 				}
-				_putchar(res + '0');
-				_putchar(',');
-			}
-			else
-			{
-				_putchar((res/10) + '0');
-				_putchar((res%10) + '0');
-				_putchar(',');
-				_putchar(' ');
+				else
+					_putchar(' ');
 			}
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
