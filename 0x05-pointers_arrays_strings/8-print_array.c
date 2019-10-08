da9625da68d8d1952ead6e21db9473cfe8b84a3d
gomba66 <danielalejo66@hotmail.com>
2019-10-08 22:10:04 +0000

    I added a program that prints all the elements of a string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - Function that prints all elements of an array
+ * @a: Array.
+ * @n: number of elements.
+ */
+void print_array(int *a, int n)
+{
+	int it = 0;
+
+	while (it < n)
+	{
+		if (it != n - 1)
+		{
+			printf("%d, ", a[it]);
+		}
+		else
+		{
+			printf("%d\n", a[it]);
+		}
+		it++;
+	}
+}
