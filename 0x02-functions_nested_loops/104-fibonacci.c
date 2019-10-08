87cc7390326feec675af12e97614a7b4da35bf70
gomba66 <danielalejo66@hotmail.com>
2019-10-08 18:18:04 +0000

    [test] Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,8 +5,10 @@
  */
 int main(void)
 {
-	unsigned int long n1 = 0, n2 = 1, count = 0, fib = 1;
+	unsigned int long n1 = 1, n2 = 2, count = 0, fib = 1;
 
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
 	while (count < 99)
 	{
 		fib = n1 + n2;
