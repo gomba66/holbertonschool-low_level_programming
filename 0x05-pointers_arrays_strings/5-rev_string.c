5ac77b894a32fab12c2f5862c8867bf443877659
gomba66 <danielalejo66@hotmail.com>
2019-10-08 21:03:47 +0000

    I added a program that replace a string in a pointer in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,26 @@
  */
 void rev_string(char *s)
 {
-	int x, y;
-	char i[1000];
-
-	while (s[x] != '\0')
+	int i = 0, y;
+	char newstr[1000];
+	while (s[i] != '\0')
 	{
-		x++;
-		y++;
+		i++;
 	}
-	y--;
+	i = i - 1;
+	y = i;
+	i = 0;
 	while (y != -1)
 	{
-		x++;
-		i = s[y];
-		y--;
+	       newstr[i] = s[y];
+	       y--;
+	       i++;
+	}
+	newstr[i] = '\0';
+	y = 0;
+	while (newstr[y] != '\0')
+	{
+		s[y] = newstr[y];
+		y++;
 	}
 }
