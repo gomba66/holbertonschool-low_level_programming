d2eee849b108f5b54758236915a3bcc5fb74be04
gomba66 <danielalejo66@hotmail.com>
2019-10-12 01:21:58 +0000

    I fixed a condition

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
+	if (size < 0)
+	{
+		return (NULL);
+	}
 	table = malloc(sizeof(hash_table_t) * size);
 	if (table == NULL)
 	{
