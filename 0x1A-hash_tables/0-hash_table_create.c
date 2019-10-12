b03aaa1c86991f58e37f22b8f8c079f29308e058
gomba66 <danielalejo66@hotmail.com>
2019-10-12 01:36:34 +0000

    I fixed the size of the malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(hash_table_t) * 1);
 	if (table == NULL)
 	{
 		return (NULL);
