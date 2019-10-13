846fd096963e87d1f80284330bd3a0cc958b7a66
gomba66 <danielalejo66@hotmail.com>
2019-10-13 18:34:00 +0000

    i fixed the conditions

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,19 +6,22 @@
  */
 char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, y = 0;
+	char *llow = "aeotl";
+	char *lup = "AEOTL";
+	char *rep = "43071";
+
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		y = 0;
+		while (llow[y] != '\0' || lup[y] != '\0')
+		{
+			if (s[i] == llow[y] || s[i] == lup[y])
+			{
+				s[i] = rep[y];
+			}
+			y++;
+		}
 		i++;
 	}
 	return (s);
