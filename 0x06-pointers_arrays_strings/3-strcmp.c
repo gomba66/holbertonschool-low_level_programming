8a715e26acde12b931b8e06cfb9b7a9051a45dbe
gomba66 <danielalejo66@hotmail.com>
2019-10-13 04:38:30 +0000

    I fixed the changes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _strcmp - Entry point
  * @s1: This is the first string
@@ -12,5 +11,5 @@ int _strcmp(char *s1, char *s2)
 
 	while ((s1[x] != '\0' && s2[x] != '\0') && (s1[x] == s2[x]))
 		x++;
-	return ((s1[0]) - (s2[0]));
+	return ((s1[x]) - (s2[x]));
 }
