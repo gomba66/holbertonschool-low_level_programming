b36d074478317071b2d2bdfe385486c49a551620
gomba66 <danielalejo66@hotmail.com>
2019-10-13 23:00:26 +0000

    I added a new function that prints numbers of the ascii table

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - This is a function that prints a number
+ * @n: Number that we need to print.
+ */
+void print_number(int n)
+{
+	int i = 1, digit = 1, power = 1;
+	unsigned int numchar, number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+		number = n;
+	}
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	while (i < digit)
+	{
+		power = power * 10;
+		i++;
+	}
+	while (power >= 1)
+	{
+		numchar = (n / power) % 10;
+		_putchar(numchar + '0');
+		power = power / 10;
+	}
+}
