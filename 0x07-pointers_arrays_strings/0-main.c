c5c12c96b846ecc6073c765d775e6d068748926f
gomba66 <danielalejo66@hotmail.com>
2019-10-15 21:11:05 +0000

    I fixed some spaces

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,25 +10,24 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-        unsigned int i;
+	unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
-
 /**
  * main - check the code for Holberton School students.
  *
