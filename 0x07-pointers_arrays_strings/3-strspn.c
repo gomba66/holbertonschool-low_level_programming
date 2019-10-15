9ad77f58fadead2abc8535e86cb2c3a67d7037c7
gomba66 <danielalejo66@hotmail.com>
2019-10-15 21:12:50 +0000

    I added some new conditios

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, a = 0, b = 0;
+	int i = 0, a = 0, b = 0, status = 0, z = 0;
 
-	while (accept[i] != '\0')
+
+	while (s[i] != '\0' && status == 0)
 	{
 		a = 0;
-		while (s[a] != '\0')
+		while (accept[a] != '\0')
 		{
-			if(s[a] == accept[i] && i < a)
+			if (accept[a] == s[i])
+			{
 				b++;
+			}
 			a++;
 		}
+		z = i + 1;
+		if (b != z)
+			status = 1;
+		else
+			status = 0;
 		i++;
 	}
 	return (b);
