3432269eaea74db327c1fe02bc74fed8f43f412b
gomba66 <danielalejo66@hotmail.com>
2019-10-16 05:14:45 +0000

    I added a new program for to verify if a string is palindrome or not

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,19 +1,46 @@
 #include "holberton.h"
 /**
- * is_palindrome - Entry point
- * This is a function for comparate 2 strings
- * @s: Is the first string
- * Return: If the strings be identical 1, otherwise return 0.
+ * size - Function for obtain the size of the string.
+ * @s: string.
+ * Return: size of the string.
+ */
+int size(char *s)
+{
+	int tsize = 0;
+
+	if (*s != '\0')
+	{
+		tsize++;
+		return (tsize + size(s + 1));
+	}
+	else
+		return (tsize);
+}
+/**
+ * comp - comp chars
+ * @s: string.
+ * @size: size.
+ * @i: iterator.
+ * Return: 1 if is palindrome / 0 in otherwise
+ */
+int comp(char *s, int size, int i)
+{
+	if (i >= size)
+		return (1);
+	else if (s[i] == s[size])
+		return (comp(s, size - 1, i + 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome - catculates size of a str
+ * @s: string
+ * Return: 1 if is palindrome / 0 in otherwise
  */
 int is_palindrome(char *s)
 {
-	if (s == '\0')
-	{
-		return (0);
-	}
-	else if (s == '\0')
-	{
-		return (1);
-	}
-	return (1);
+	int sz = size(s);
+	int i = 0;
+
+	return (comp(s, sz - 1, i));
 }
