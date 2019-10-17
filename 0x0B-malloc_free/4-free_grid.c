ac2934ba26384b6cfdecfb5b2cfd8f31003efdf1
gomba66 <danielalejo66@hotmail.com>
2019-10-17 00:10:34 -0500

    I fixed the betty style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,13 +9,13 @@ void free_grid(int **grid, int height)
 	int h = 0;
 
 	if (grid != NULL && height > 0)
-	  {
-	    while (h <= height)
-	      {
-		free(grid[h]);
-		h++;
-	      }
-	    free(grid);
-	  }
+	{
+		while (h <= height)
+		{
+			free(grid[h]);
+			h++;
+		}
+		free(grid);
+	}
 
 }
