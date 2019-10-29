1650971450dcc73cd4dc7e3cfa8fc6be390c8a50
Gomba66 <danielalejo66@hotmail.com>
2019-10-29 18:54:46 +0000

    I added a new function that free the memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - Function that freezed all the linked list.
+ * @head: head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
