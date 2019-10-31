0f5c68ab7dab964fd7992de2cc607cdc70e800f1
Gomba66 <danielalejo66@hotmail.com>
2019-10-31 04:58:57 +0000

    [EMPTY] I added an empty function

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * selection_sort - Function that sorts an array of integer in
+ * ascending order using the selection algorithm.
+ * @array: Array.
+ * @size: Size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
