6b6d97f7baed21bcb010218dfb19af7e1a2a55be
Gomba66 <danielalejo66@hotmail.com>
2019-10-31 15:49:13 +0000

    [Task 0][Fix] I implemented the algorithm

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -7,8 +7,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	(void)array;
-	(void)size;
+	size_t x = 0;
+	size_t y = 0;
+	int temp = 0;
 
-
+	for (x = 0; x < size - 1; x++)
+		for (y = 0; y < (size - x) - 1; y++)
+		{
+			if (array[y] > array[y + 1])
+			{
+				temp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = temp;
+				print_array(array, size);
+			}
+		}
 }
