bdc4c6c2cddb14f26c2adb1d8c00e219a0b2d0d1
Gomba66 <danielalejo66@hotmail.com>
2019-10-31 04:42:41 +0000

    [EMPTY] I added an empty function

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * bubble_sort - Function that sort an array of integer in ascending order
+ * using the Bubble sor algorithm.
+ * @array: Array that will be sorted.
+ * @size: Size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+
+
+}
