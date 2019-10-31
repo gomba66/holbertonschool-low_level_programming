4a1c77dc2f2721f77075c487dfbbeb4f05521d9c
Gomba66 <danielalejo66@hotmail.com>
2019-10-31 05:10:19 +0000

    [EMPTY] I added an empty function

---
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * shell_sort - function that sorts an array of integers
+ * in ascending order using the Shell sort algorithm,
+ * using the Knuth sequence.
+ * @array: Array.
+ * @size: Size of the array.
+ */
+void shell_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+
+}
