179f2ab9bd094b2c970de7cde3d12af1230448b7
Gomba66 <danielalejo66@hotmail.com>
2019-10-31 04:53:02 +0000

    [EMPTY] I added an empty function

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Function that sorts a doubly linked list of integers
+ * in ascending order using the inserion sort algorithm.
+ * @list: double linked list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	(void)list;
+}
