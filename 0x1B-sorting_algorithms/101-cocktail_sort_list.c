869e9b9017684f7537682618cf63e814bdf548da
Gomba66 <danielalejo66@hotmail.com>
2019-10-31 05:16:28 +0000

    [EMPTY] I added an empty function

---
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the Cocktail shaker sort algorithm.
+ * @list: List.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	(void)list;
+}
