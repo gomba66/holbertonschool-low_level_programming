a00ae81edfcc2b27ab5e6227b0c662c0da2399dc
Gomba66 <danielalejo66@hotmail.com>
2019-11-16 06:24:44 +0000

    I added the island task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,9 +5,73 @@
 def island_perimeter(grid):
     """ Function that determine the perimeter of a grid"""
     total = 0
-    for a in range(len(grid)):
-        for b in range(len(grid[a])):
-            if grid[a][b] == 1:
-                total = total + 1
-    total = ((((len(grid[a])) * (len(grid))) - total) // 2)
+    for b in range(len(grid)):
+        for a in range(len(grid[b])):
+            if (a == 0) and (b == 0):
+                if grid[b][a] == 1:
+                    if grid[b][a + 1] == 0:
+                        total = total + 1
+                    if grid[b + 1][a] == 0:
+                        total = total + 1
+            if (a == len(grid[b]) - 1) and b == 0:
+                if grid[b][a] == 1:
+                    if grid[b + 1][a] == 0:
+                        total = total + 1
+                    if grid[b][a - 1] == 0:
+                        total = total + 1
+            if a == 0 and b == (len(grid) - 1):
+                if grid[b][a] == 1:
+                    if grid[b][a + 1] == 0:
+                        total = total + 1
+                    if grid[b - 1][a] == 0:
+                        total = total + 1
+            if b == (len(grid) - 1) and a == (len(grid[b]) - 1):
+                if grid[b][a] == 1:
+                    if grid[b - 1][a] == 0:
+                        total = total + 1
+                    if grid[b][a - 1] == 0:
+                        total = total + 1
+            if (b == 0 and a > 0) and a < (len(grid[b]) - 1):
+                if grid[b][a] == 1:
+                    if grid[b][a - 1] == 0:
+                        total = total + 1
+                    if grid[b + 1][a] == 0:
+                        total = total + 1
+                    if grid[b][a + 1] == 0:
+                        total = total + 1
+            if (b == 0 and b < (len(grid) - 1)) and (a < len(grid[b]) - 1):
+                if grid[b][a] == 1:
+                    if grid[b - 1][a] == 0:
+                        total = total + 1
+                    if grid[b][a + 1] == 0:
+                        total = total + 1
+                    if grid[b + 1][a] == 0:
+                        total = total + 1
+            if b > 0 and (a == len(grid[b]) -1):
+                if grid[b][a] == 1:
+                    if grid[b - 1][a] == 0:
+                        total = total + 1
+                    if grid[b][a - 1] == 0:
+                        total = total + 1
+                    if grid[b + 1][a] == 0:
+                        total = total + 1
+            if (b == len(grid) - 1) and a > 0 and a < len(grid[b]) - 1:
+                if grid[b][a] == 1:
+                    if grid[b][a - 1] == 0:
+                        total = total + 1
+                    if grid[b - 1][a] == 0:
+                        total = total + 1
+                    if grid[b][a + 1] == 0:
+                        total = total + 1
+            else:
+                if grid[b][a] == 1:
+                    if grid[b][a - 1] == 0:
+                        total = total + 1
+                    if grid[b][a + 1] == 0:
+                        total = total + 1
+                    if grid[b - 1][a] == 0:
+                        total = total + 1
+                    if grid[b + 1][a] == 0:
+                        total = total + 1
+
     return total
