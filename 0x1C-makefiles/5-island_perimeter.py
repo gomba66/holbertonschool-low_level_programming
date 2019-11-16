64f00cfd0ace44055aed4325607306e7621f2e57
Gomba66 <danielalejo66@hotmail.com>
2019-11-16 07:16:22 +0000

    i fixed the edge cases

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,6 +7,7 @@ def island_perimeter(grid):
     total = 0
     for b in range(len(grid)):
         for a in range(len(grid[b])):
+            #esquina izquierda
             if (a == 0) and (b == 0):
                 if grid[b][a] == 1:
                     total = total + 2
@@ -14,37 +15,43 @@ def island_perimeter(grid):
                         total = total + 1
                     if grid[b + 1][a] == 0:
                         total = total + 1
-            if (a == len(grid[b]) - 1) and b == 0:
+            #esquina derecha
+            elif (a == len(grid[b]) - 1) and b == 0:
                 if grid[b][a] == 1:
                     total = total + 2
                     if grid[b + 1][a] == 0:
                         total = total + 1
                     if grid[b][a - 1] == 0:
                         total = total + 1
-            if a == 0 and b == (len(grid) - 1):
+            #esquina inferior izquierda
+            elif a == 0 and b == (len(grid) - 1):
                 if grid[b][a] == 1:
                     total = total + 2
                     if grid[b][a + 1] == 0:
                         total = total + 1
                     if grid[b - 1][a] == 0:
                         total = total + 1
-            if b == (len(grid) - 1) and a == (len(grid[b]) - 1):
+            #esquina inferior derecha
+            elif b == (len(grid) - 1) and a == (len(grid[b]) - 1):
                 if grid[b][a] == 1:
                     total = total + 2
                     if grid[b - 1][a] == 0:
                         total = total + 1
                     if grid[b][a - 1] == 0:
                         total = total + 1
-            if (b == 0 and a > 0) and a < (len(grid[b]) - 1):
+            #borde superior
+            elif (b == 0 and a > 0) and a < (len(grid[b]) - 1):
                 if grid[b][a] == 1:
-                    total = total +1
+                    total = total + 1
                     if grid[b][a - 1] == 0:
                         total = total + 1
                     if grid[b + 1][a] == 0:
                         total = total + 1
                     if grid[b][a + 1] == 0:
                         total = total + 1
-            if (b == 0 and b < (len(grid) - 1)) and (a < len(grid[b]) - 1):
+            #borde izquierdo
+            elif (b > 0 and b < (len(grid) - 1))
+            and ((a == 0) and a < len(grid[b]) - 1):
                 if grid[b][a] == 1:
                     total = total + 1
                     if grid[b - 1][a] == 0:
@@ -53,7 +60,7 @@ def island_perimeter(grid):
                         total = total + 1
                     if grid[b + 1][a] == 0:
                         total = total + 1
-            if b > 0 and (a == len(grid[b]) -1):
+            elif (b > 0 and (b < len(grid) - 1)) and (a == len(grid[b]) - 1):
                 if grid[b][a] == 1:
                     total = total + 1
                     if grid[b - 1][a] == 0:
@@ -62,7 +69,7 @@ def island_perimeter(grid):
                         total = total + 1
                     if grid[b + 1][a] == 0:
                         total = total + 1
-            if (b == len(grid) - 1) and a > 0 and a < len(grid[b]) - 1:
+            elif (b == len(grid) - 1) and a > 0 and a < len(grid[b]) - 1:
                 if grid[b][a] == 1:
                     total = total + 1
                     if grid[b][a - 1] == 0:
@@ -71,7 +78,8 @@ def island_perimeter(grid):
                         total = total + 1
                     if grid[b][a + 1] == 0:
                         total = total + 1
-            else:
+            elif (b > 0 and b < len(grid) - 1) and
+            (a > 0 and a < len(grid[b]) - 1):
                 if grid[b][a] == 1:
                     if grid[b][a - 1] == 0:
                         total = total + 1
@@ -81,5 +89,4 @@ def island_perimeter(grid):
                         total = total + 1
                     if grid[b + 1][a] == 0:
                         total = total + 1
-
     return total
