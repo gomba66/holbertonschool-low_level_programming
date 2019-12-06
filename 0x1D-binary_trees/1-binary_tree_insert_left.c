e9f57d6dfc65c5d40ed40a7bb3205dbe96eba29e
Gomba66 <danielalejo66@hotmail.com>
2019-12-06 07:28:40 +0000

    I fixed an error

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -11,14 +11,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
+
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 
 	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
