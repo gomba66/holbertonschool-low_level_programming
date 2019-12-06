e305551f602cc21e0a3ab022e79cb74861796df4
Gomba66 <danielalejo66@hotmail.com>
2019-12-06 08:38:09 +0000

    I fixed a problem

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -5,9 +5,12 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->right != NULL)
-		binary_tree_delete(tree->right);
-	if (tree->left != NULL)
-		binary_tree_delete(tree->left);
-	free(tree);
+	if (tree != NULL)
+	{
+		if (tree->right != NULL)
+			binary_tree_delete(tree->right);
+		if (tree->left != NULL)
+			binary_tree_delete(tree->left);
+		free(tree);
+	}
 }
