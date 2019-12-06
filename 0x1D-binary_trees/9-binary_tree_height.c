937ccfcbc561219544bef63ea665503698c2aabe
Gomba66 <danielalejo66@hotmail.com>
2019-12-06 17:50:23 +0000

    I fixed the function using recursion

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 /**
  * binary_tree_height - Function that measures the height of a binary tree.
  * @tree: Tree.
@@ -6,13 +7,21 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t left_side = 0;
+	size_t right_side = 0;
 
 	if (tree == NULL)
 		return (0);
+
+
 	if (tree->left != NULL)
-		count++;
+		left_side = 1 + binary_tree_height(tree->left);
 	if (tree->right != NULL)
-		count++;
-	return (count);
+		right_side = 1 + binary_tree_height(tree->right);
+	if (right_side > left_side)
+	{
+		return (right_side);
+	}
+	else
+		return (left_side);
 }
