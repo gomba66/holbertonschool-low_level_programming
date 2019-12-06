e9b283df5aa1c7551f227453fd538534d4750944
Gomba66 <danielalejo66@hotmail.com>
2019-12-06 22:25:04 +0000

    I added a new function

---
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - Function that says you the number of leaves.
+ * @tree: Tree.
+ * Return: The number of leaves of the tree.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t total1 = 0;
+	size_t total2 = 1;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+		total1 = (binary_tree_leaves(tree->left));
+	if (tree->right != NULL)
+		total2 = (binary_tree_leaves(tree->right));
+	return (total1 + total2);
+}
