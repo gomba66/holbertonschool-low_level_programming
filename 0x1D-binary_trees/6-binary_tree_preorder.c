4f1828fb1d8007826b731482579d050ab6327279
Gomba66 <danielalejo66@hotmail.com>
2019-12-06 14:34:50 +0000

    I fixed the error

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -8,14 +8,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func != NULL)
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree != NULL)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
-	}
-	else
-	{
-		return;
 	}
 }
