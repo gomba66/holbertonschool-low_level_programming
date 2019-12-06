2f97203298555d54bafa1f8fd016fad6d08bcdc6
Gomba66 <danielalejo66@hotmail.com>
2019-12-06 22:42:02 +0000

    I changed somethign

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -6,18 +6,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t total1 = 0;
-	size_t total2 = 1;
+	size_t total2 = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	if (tree->right != NULL)
+	if (tree->right != NULL && tree->left != NULL)
 		total2 = (binary_tree_leaves(tree->right));
-
-	if (tree->left != NULL)
-		total1 = (binary_tree_leaves(tree->left));
-	return (total1 + total2);
+	return (total2 + 1);
 }
