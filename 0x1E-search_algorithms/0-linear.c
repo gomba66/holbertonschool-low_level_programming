67cb522f36c2f809e72967b4b849ad0d2d08bbab
gomba66 <danielalejo66@hotmail.com>
2020-01-17 02:46:38 +0000

    I fixed the conditional

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -29,9 +29,9 @@ int linear_search(int *array, size_t size, int value)
 		{
 			count++;
 		}
+		if (array[count] == '\0')
+			return (-1);
 	}
-	if (array[count] == '\0')
-		return (-1);
 
 	return (count);
 }
