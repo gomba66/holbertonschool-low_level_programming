5c28c5eea80cf60b23eb0ab691103cd9eac0e7cf
gomba66 <danielalejo66@hotmail.com>
2020-03-10 19:59:20 +0000

    I added a new task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,13 @@
-#ifndef SEARCH_ALG
-#define SEARCH_ALG
-#include <stdio.h>
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
 #include <stdlib.h>
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
 #endif
